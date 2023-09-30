c06d5986e947804a5dd43fec70ad40aa74d35148
Arendelle <2298157027@qq.com>
2023-09-30 16:05:01 +0800

    移动同步

---
+++ Src/Client/Assets/Scripts/UI/UIMiniMap/MiniMap.cs
@@ -0,0 +1,47 @@
+﻿using Managers;
+using Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMap : MonoBehaviour {
+    public Collider miniMapBoundingBox;
+    public Image miniMap;
+    public Image arrow;
+    public Text mapName;
+
+    private Transform playerTransform;
+	void Start () {
+        this.MapInit();
+	}
+
+    void MapInit()
+    {
+        this.mapName.text = User.Instance.CurrentMapData.Name;
+        if (this.miniMap.overrideSprite == null)
+        {
+            this.miniMap.overrideSprite = MiniMapManager.Instance.LoadCurrentMinimap();
+        }
+        this.miniMap.SetNativeSize();
+        this.miniMap.transform.localPosition = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.playerTransform = User.Instance.CurrentCharacterObject.transform;
+        float realWidth = miniMapBoundingBox.bounds.size.x;
+        float realHeight = miniMapBoundingBox.bounds.size.z;
+
+        float relaX = playerTransform.position.x - miniMapBoundingBox.bounds.min.x;
+        float relaY = playerTransform.position.z - miniMapBoundingBox.bounds.min.z;
+
+        float pivotX = relaX / realWidth;
+        float pivotY = relaY / realHeight;
+
+        this.miniMap.rectTransform.pivot = new Vector2(pivotX, pivotY);
+        this.miniMap.rectTransform.localPosition = Vector2.zero;
+        this.arrow.transform.eulerAngles = new Vector3(0, 0, -playerTransform.eulerAngles.y);
+    }
+
+}
---
+++ Src/Server/GameServer/GameServer/Managers/EntityManager.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using GameServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Managers
+{
+    class EntityManager:Singleton<EntityManager>
+    {
+        private int idx = 0;
+        public List<Entity> AllEntities = new List<Entity>();
+        public Dictionary<int, List<Entity>> MapEntites = new Dictionary<int, List<Entity>>();
+
+        public void AddEntity(int mapId,Entity entity)
+        {
+            AllEntities.Add(entity);
+            entity.EntityData.Id = ++this.idx;
+
+            List<Entity> entities = null;
+            if(!MapEntites.TryGetValue(mapId,out entities))
+            {
+                entities = new List<Entity>();
+                MapEntites[mapId] = entities;
+            }
+            entities.Add(entity);
+        }
+
+        public void RemoveEntity(int mapId,Entity entity)
+        {
+            this.AllEntities.Remove(entity);                                                                 
+            this.MapEntites[mapId].Remove(entity);
+        }
+    }
+}
