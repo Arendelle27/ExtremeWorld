b4eb6deb0f33b5ab6ae8a387105dc7487287db99
Arendelle <2298157027@qq.com>
2023-11-23 20:37:14 +0800

    装备系统

---
+++ Src/Client/Assets/Scripts/Managers/EquipManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Models;
+using Services;
+using SkillBridge.Message;
+using UnityEngine;
+
+namespace Managers
+{
+    public class EquipManager : Singleton<EquipManager>
+    {
+        public delegate void OnEquipChanageHandler();
+
+        public event OnEquipChanageHandler OnEquipChanaged;
+
+        public Item[] Equips = new Item[(int)EquipSlot.SlotMax];
+
+        byte[] Data;
+
+        unsafe public void Init(byte[] data)
+        {
+            this.Data = data;
+            this.ParseEquipData(data);
+        }
+
+        public bool Contains(int equipId)
+        {
+            for(int i=0;i<this.Equips.Length;i++)
+            {
+                if (Equips[i] != null && Equips[i].Id == equipId)
+                    return false;
+            }
+            return false;
+        }
+
+        public Item GetEquip(EquipSlot slot)
+        {
+            return Equips[(int)slot];
+        }
+
+        unsafe void ParseEquipData(byte[] data)
+        {
+            fixed(byte* pt=this.Data)
+            {
+                for(int i=0;i<this.Equips.Length;i++)
+                {
+                    int itemId = *(int*)(pt + i * sizeof(int));
+                    if (itemId > 0)
+                        Equips[i] = ItemManager.Instance.Items[itemId];
+                    else
+                        Equips[i] = null;
+                }
+            }
+        }
+
+        unsafe public byte[] GetEquipData()
+        {
+            fixed(byte* pt=this.Data)
+            {
+                for(int i=0;i<(int)EquipSlot.SlotMax;i++)
+                {
+                    int* itemId=(int*)(pt + i * sizeof(int));
+                    if (Equips[i] == null)
+                        *itemId = 0;
+                    else
+                        *itemId = Equips[i].Id;
+                }
+            }
+            return this.Data;
+        }
+
+        public void EquipItem(Item equip)
+        {
+            ItemService.Instance.SendEquipItem(equip, true);
+        }
+
+        public void UnEquipItem(Item equip)
+        {
+            ItemService.Instance.SendEquipItem(equip, false);
+        }
+
+        public void OnEquipItem(Item equip)
+        {
+            if(this.Equips[(int)equip.EquipInfo.Slot]!=null&&this.Equips[(int)equip.EquipInfo.Slot].Id==equip.Id)
+            {
+                return;
+            }
+            this.Equips[(int)equip.EquipInfo.Slot] = ItemManager.Instance.Items[equip.Id];
+
+            if (OnEquipChanaged != null)
+                OnEquipChanaged();
+        }
+
+        public void OnUnEquipItem(EquipSlot slot)
+        {
+            if(this.Equips[(int)slot]!=null)
+            {
+                this.Equips[(int)slot] = null;
+                if (OnEquipChanaged != null)
+                    OnEquipChanaged();
+            }
+        }
+    }
+}
---
+++ Src/Server/GameServer/GameServer/Models/Item.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using Common.Data;
+using GameServer.Entities;
+using GameServer.Services;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GameServer.Models
+{
+    class Item
+    {
+        TCharacterItem dbItem;
+
+        public int ItemID;
+
+        public int Count;
+
+        public Item(TCharacterItem item)
+        {
+            this.dbItem = item;
+
+            this.ItemID = (short)item.ItemID;
+            this.Count = (short)item.ItemCount;
+        }
+
+        public void Add(int count)
+        {
+            this.Count += count;
+            dbItem.ItemCount = this.Count;
+        }
+
+        public void Remove(int count)
+        {
+            this.Count -= count;
+            dbItem.ItemCount = this.Count;
+        }
+
+        public bool Use(int count=1)
+        {
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID:{0},Count:{1}", this.ItemID, this.Count);
+        }
+    }
+}
